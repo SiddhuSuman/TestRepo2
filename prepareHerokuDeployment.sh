a575b7345ba02585b3b60735b4a490fa28a2b8b9
Siddaharth Suman <siddaharthsuman@gmail.com>
2015-03-25 17:35:24 +0530

    empty

--- prepareHerokuDeployment.sh
+++ prepareHerokuDeployment.sh
@@ -11,14 +11,12 @@ function build-dart {
 }
 
 function commit-changes {
-
 	echo "commit-changes"
 	git config --global user.email "support@drone.io"
 	git config --global user.name "Drone Server"
 	git status 
 	git add .
 	git commit -m "add compiled and prefixed files" || { echo 'commit-changes' ; exit 1; }
-
 }
 
 get-dart-dependencies 
